7589f928e02292a689c464103da30404a0ea1eb5
Howard Butler <howard@hobu.co>
2018-10-26 21:48:14 -0500

    fix gdal-related copypasta

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,6 @@ DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/gdal-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/geotiff-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/geotiff-deactivate.sh
 
